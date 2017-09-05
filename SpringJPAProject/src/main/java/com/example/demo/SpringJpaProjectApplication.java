68bd8f1866615a6df3f834e19dd40e7ea552de7b
markaan <anand.markandeya@verizonwireless.com>
2017-09-04 21:48:15 -0400

    updated

--- SpringJPAProject/src/main/java/com/example/demo/SpringJpaProjectApplication.java
+++ SpringJPAProject/src/main/java/com/example/demo/SpringJpaProjectApplication.java
@@ -8,7 +8,7 @@ public class SpringJpaProjectApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringJpaProjectApplication.class, args);
-		System.out.println("Spring App ==== ");
+		
 		
 	}
 }
